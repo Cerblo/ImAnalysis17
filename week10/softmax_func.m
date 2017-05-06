a50753f85f7aac90c3abecf9ef498fce9bdfafa4
Melissande <melissandemachefer@gmail.com>
2017-05-06 12:01:21 +0200

    Delete softmax_func.m

--- week10/softmax_func.m
+++
@@ -1,9 +0,0 @@
-function z = softmax_func( a )
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
-    col_sum = sum(exp(a),2);
-    sums = repmat(col_sum, 1, size(a,2));
-    z = exp(a)./sums;
-
-end
-
